b428482b105b61e6f32d0a90eb10685c3b4fe3ab
Carlos Soria <cultome@gmail.com>
2014-07-28 13:43:23 -0500

    Initial commit

---
+++ app/js/app.js
@@ -0,0 +1,36 @@
+/* jshint strict: true */
+
+angular.module('CEDEK', ['ngRoute']).
+config(['$routeProvider', function($routeProvider) {
+  'use strict';
+  $routeProvider.when('/personas/agregar', {templateUrl: 'partials/persona/agregar.html', controller: 'PeopleCtrl'});
+
+  $routeProvider.when('/cursos/agregar', {templateUrl: 'partials/curso/agregar.html', controller: 'CourseCtrl'});
+  $routeProvider.when('/curso/activo/:courseId', {templateUrl: 'partials/curso/activo.html', controller: 'CourseCtrl'});
+
+  $routeProvider.when('/dashboard', {templateUrl: 'partials/dashboard.html', controller: 'DashboardCtrl'});
+  $routeProvider.otherwise({redirectTo: '/dashboard'});
+}]);
+
+var app = angular.module('CEDEK');
+
+app.controller('PeopleCtrl', ['$scope',
+  function($scope){
+    'use strict';
+    return {};
+  }]
+);
+
+app.controller('DashboardCtrl', ['$scope',
+  function($scope){
+    'use strict';
+    return {};
+  }]
+);
+
+app.directive('navigation', function(){
+  return {
+    replace: true,
+    templateUrl: 'partials/navigation.html'
+  };
+});
