ebe8f549be352397e828d1d538e62c5de11f56c7
Carlos Soria <cultome@gmail.com>
2014-07-28 14:31:13 -0500

    Add listing people and courses

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,9 @@ angular.module('CEDEK', ['ngRoute']).
 config(['$routeProvider', function($routeProvider) {
   'use strict';
   $routeProvider.when('/personas/agregar', {templateUrl: 'partials/persona/agregar.html', controller: 'PeopleCtrl'});
+  $routeProvider.when('/personas/listar', {templateUrl: 'partials/persona/listar.html', controller: 'PeopleCtrl'});
 
+  $routeProvider.when('/cursos/listar', {templateUrl: 'partials/curso/listar.html', controller: 'CourseCtrl'});
   $routeProvider.when('/cursos/agregar', {templateUrl: 'partials/curso/agregar.html', controller: 'CourseCtrl'});
   $routeProvider.when('/curso/activo/:courseId', {templateUrl: 'partials/curso/activo.html', controller: 'CourseCtrl'});
 
@@ -15,6 +17,13 @@ config(['$routeProvider', function($routeProvider) {
 var app = angular.module('CEDEK');
 
 app.controller('PeopleCtrl', ['$scope',
+  function($scope){
+    'use strict';
+    return {};
+  }]
+);
+
+app.controller('CourseCtrl', ['$scope',
   function($scope){
     'use strict';
     return {};
