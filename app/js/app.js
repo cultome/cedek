55c47038cccea6cb55cc2a4b912c340038af8a0d
Carlos Soria <cultome@gmail.com>
2014-07-29 10:28:28 -0500

    Complete user information

--- app/js/app.js
+++ app/js/app.js
@@ -18,25 +18,42 @@ config(['$routeProvider', function($routeProvider) {
 
 var app = angular.module('CEDEK');
 
-app.controller('PeopleCtrl', ['$scope', '$routeParams',
-  function($scope, $routeParams){
+
+/*
+ *   Controller
+ */
+app.controller('PeopleCtrl', ['$scope', '$routeParams', 'PeopleService', 'CourseService', 'CatalogService',
+  function($scope, $routeParams, PeopleService, CourseService, CatalogService){
     'use strict';
 
     $scope.courseId = "";
     $scope.studentId = "";
-    $scope.selectedStudent = {"name": ""};
+    $scope.selectedStudent = {
+      "name": "",
+      "hasScholarship": false,
+      "scholarshipPercent": 0,
+      "phones": [{
+        "number": "",
+        "type": 1
+      }]
+    };
+    $scope.phoneTypes = CatalogService.phoneTypes();
+    $scope.students = PeopleService.getStudents();
+    $scope.courses = CourseService.getCurrentCourses();
 
-    $scope.students = [
-      {"id": 1, "name": "Susana Alvarado", "enrollments": [{"id": 1, "name": "Curso II"}], "reserves": [{"id": 2, "name": "Curso IV"}], "previous": [{"id": 3, "name": "Curso I"}]}
-    ];
-
-    $scope.courses = [
-      {"id": 1, "status": "Activo", "name": "Esperanto Avanzado"},
-      {"id": 2, "status": "Abierto", "name": "Curso IV"}
-    ];
+    $scope.getPhoneTypeName = function(id){
+      return $scope.phoneTypes.filter(function(elem){ return elem.id === id; })[0].name;
+    };
 
     $scope.selectPerson = function(id){
       $scope.selectedStudent = $scope.students.filter(function(elem){ return elem.id == id; })[0];
+    };
+
+    $scope.addAnotherPhone = function(){
+      $scope.selectedStudent.phones.unshift({
+        "number": "",
+        "type": 1
+      });
     };
 
     if($routeParams.personId){
@@ -46,29 +63,21 @@ app.controller('PeopleCtrl', ['$scope', '$routeParams',
   }]
 );
 
-app.controller('CourseCtrl', ['$scope', '$routeParams',
-  function($scope, $routeParams){
+app.controller('CourseCtrl', ['$scope', '$routeParams', 'PeopleService', 'CourseService',
+  function($scope, $routeParams, PeopleService, CourseService){
     'use strict';
 
     $scope.selectedCourse = null;
     $scope.selectedStudent = null;
 
-    $scope.courses = [
-      {
-      "id": 1,
-      "name": "Homeopatia II",
-      "begin": "4 de junio 2014",
-      "end": "5 de enero 2015",
-      "schedule": "Jueves 18:00",
-      "students": [{"id": 1, "name": "Susana Alvarado"}, {"id": 2, "name": "Alfredo Alvarado"}, {"id": 3, "name": "Carlos Soria"}]
-    }];
+    $scope.courses = CourseService.getCourses();
 
     $scope.selectStudent = function(id){
-      $scope.selectedStudent = {"id": 1, "name": "Susana Alvarado", "enrollments": [{"id": 1, "name": "Curso II"}], "reserves": [{"id": 2, "name": "Curso IV"}], "previous": [{"id": 3, "name": "Curso I"}]};
+      $scope.selectedStudent = PeopleService.getStudent(id);
     };
 
     $scope.selectCourse = function(id){
-      $scope.selectedCourse = $scope.courses.filter(function(elem){ return elem.id == id; })[0];
+      $scope.selectedCourse = CourseService.getCourse(id)
     };
 
     if($routeParams.courseId){
@@ -91,6 +100,10 @@ app.controller('DashboardCtrl', ['$scope',
   }]
 );
 
+/*
+ *   Directive
+ */
+
 app.directive('navigation', function(){
   return {
     replace: true,
@@ -98,10 +111,57 @@ app.directive('navigation', function(){
   };
 });
 
-app.directive('editable', function(){
+/*
+ *   Factory
+ */
+app.factory('PeopleService', [function(){
   return {
-    link: function(scope, element, attrs) {
-      $(element).click(function(evt){ alert("Editable!"); });
+    getStudents: function(){
+      return [
+        {"id": 1, "name": "Susana Alvarado", "enrollments": [{"id": 1, "name": "Curso II"}], "reserves": [{"id": 2, "name": "Curso IV"}], "previous": [{"id": 3, "name": "Curso I"}]},
+        {"id": 2, "name": "Noel Soria", "enrollments": [{"id": 1, "name": "Curso I"}]}
+      ];
+    },
+
+    getStudent: function(id){
+      return this.getStudents().filter(function(elem){ return elem.id === id; });
+    },
+  };
+}]);
+
+app.factory('CatalogService', [function(){
+  return {
+    phoneTypes: function(){
+      return [
+        {"id": 1, "name": "Casa"},
+        {"id": 2, "name": "Movil"},
+        {"id": 3, "name": "Oficina"}
+      ];
     }
   };
-});
+}]);
+
+app.factory('CourseService', [function(){
+  return {
+    getCourse: function(id){
+      return this.getCourses().filter(function(elem){ return elem.id === id; })[0];
+    },
+    getCourses: function(){
+      return [
+        {"id": 1, "name": "Homeopatia II", "begin": "4 de junio 2014", "end": "5 de enero 2015", "schedule": "Jueves 18:00", "students": [{"id": 1, "name": "Susana Alvarado"}, {"id": 2, "name": "Alfredo Alvarado"}]},
+        {"id": 2, "name": "Biomagnetismo", "begin": "23 de octubre 2014", "end": "5 de enero 2015", "schedule": "Jueves 18:00", "students": [{"id": 1, "name": "Susana Alvarado"}, {"id": 3, "name": "Carlos Soria"}]},
+        {"id": 3, "name": "Curso I", "begin": "17 de abril 2014", "end": "5 de enero 2015", "schedule": "Jueves 18:00", "students": [{"id": 2, "name": "Alfredo Alvarado"}, {"id": 3, "name": "Carlos Soria"}]},
+        {"id": 4, "name": "Curso II", "begin": "6 de enero 2014", "end": "5 de enero 2015", "schedule": "Jueves 18:00", "students": [{"id": 3, "name": "Carlos Soria"}]},
+        {"id": 5, "name": "Curso III", "begin": "20 de marzo 2014", "end": "5 de enero 2015", "schedule": "Jueves 18:00", "students": [{"id": 2, "name": "Alfredo Alvarado"}]},
+        {"id": 6, "name": "Curso IV", "begin": "30 de septiembre 2014", "end": "5 de enero 2015", "schedule": "Jueves 18:00", "students": [{"id": 1, "name": "Susana Alvarado"}]}
+      ];
+    },
+
+    getCurrentCourses: function(){
+      return [
+        {"id": 1, "status": "Activo", "name": "Esperanto Avanzado"},
+        {"id": 2, "status": "Abierto", "name": "Curso IV"}
+      ];
+    },
+  };
+}]);
