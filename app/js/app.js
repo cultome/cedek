7540f8a67d4375e96089f8aa02e245bd2169f21b
Carlos Soria <cultome@gmail.com>
2014-07-29 14:20:14 -0500

    Details in client comments

--- app/js/app.js
+++ app/js/app.js
@@ -22,7 +22,7 @@ var app = angular.module('CEDEK');
 /*
  *   Controller
  */
-app.controller('PeopleCtrl', ['$scope', '$routeParams', 'PeopleService', 'CourseService', 'CatalogService',
+app.controller('PeopleCtrl', ['$scope', '$routeParams', 'PeopleService', 'CourseService', 'CatalogService', 
   function($scope, $routeParams, PeopleService, CourseService, CatalogService){
     'use strict';
 
@@ -35,7 +35,8 @@ app.controller('PeopleCtrl', ['$scope', '$routeParams', 'PeopleService', 'Course
       "phones": [{
         "number": "",
         "type": 1
-      }]
+      }],
+      "debts": null
     };
     $scope.phoneTypes = CatalogService.phoneTypes();
     $scope.students = PeopleService.getStudents();
@@ -49,6 +50,10 @@ app.controller('PeopleCtrl', ['$scope', '$routeParams', 'PeopleService', 'Course
       $scope.selectedStudent = $scope.students.filter(function(elem){ return elem.id == id; })[0];
     };
 
+    $scope.hasAcademicHistory = function(){
+      return $scope.selectedStudent.enrollments || $scope.selectedStudent.reserves || $scope.selectedStudent.previous;
+    };
+
     $scope.addAnotherPhone = function(){
       $scope.selectedStudent.phones.unshift({
         "number": "",
@@ -56,9 +61,34 @@ app.controller('PeopleCtrl', ['$scope', '$routeParams', 'PeopleService', 'Course
       });
     };
 
-    if($routeParams.personId){
+    /*
+     * Repetidos
+     */
+    $scope.changeToLatePartialPayment = function(studentId){
+      $scope.selectedStudent.debts.filter(function(elem){ return elem.id === studentId; })[0].paymentType = 2;
+      $("#latePayment" + studentId).attr("type", "number");
+    };
 
-      $scope.selectPerson($routeParams.personId);
+    $scope.changeToLatePayment = function(studentId){
+      $scope.selectedStudent.debts.filter(function(elem){ return elem.id === studentId; })[0].paymentType = 3;
+      $("#latePayment" + studentId).attr("type", "date");
+    };
+
+    $scope.toggleLatePaymentOptions = function(studentId){
+      console.log(studentId);
+      var section = $("#latePaymentOption" + studentId);
+      if(section.css("display") === "none"){
+        section.css("display", "");
+      } else {
+        section.css("display", "none");
+      }
+    };
+    /*
+     * /Repetidos
+     */
+
+    if($routeParams.personId){
+      $scope.selectPerson(parseInt($routeParams.personId));
     }
   }]
 );
@@ -69,6 +99,8 @@ app.controller('CourseCtrl', ['$scope', '$routeParams', 'PeopleService', 'Course
 
     $scope.selectedCourse = null;
     $scope.selectedStudent = null;
+    $scope.studentsWithScholarship = [PeopleService.getStudent(1)];
+    $scope.studentsWithPendingPayments = [PeopleService.getStudent(1), PeopleService.getStudent(2)];
 
     $scope.courses = CourseService.getCourses();
 
@@ -80,8 +112,46 @@ app.controller('CourseCtrl', ['$scope', '$routeParams', 'PeopleService', 'Course
       $scope.selectedCourse = CourseService.getCourse(id)
     };
 
+    $scope.changeToPartialPayment = function(studentId){
+      $scope.selectedCourse.students.filter(function(elem){ return elem.id === studentId; })[0].paymentType = 2;
+      $("#payment" + studentId).attr("type", "number");
+    };
+
+    $scope.changeToLaterPayment = function(studentId){
+      $scope.selectedCourse.students.filter(function(elem){ return elem.id === studentId; })[0].paymentType = 3;
+      $("#payment" + studentId).attr("type", "date");
+    };
+
+    $scope.togglePaymentOptions = function(studentId){
+      var section = $("#paymentOption" + studentId);
+      if(section.css("display") === "none"){
+        section.css("display", "");
+      } else {
+        section.css("display", "none");
+      }
+    };
+
+    $scope.changeToLatePartialPayment = function(studentId){
+      $scope.studentsWithPendingPayments.filter(function(elem){ return elem.id === studentId; })[0].paymentType = 2;
+      $("#latePayment" + studentId).attr("type", "number");
+    };
+
+    $scope.changeToLatePayment = function(studentId){
+      $scope.studentsWithPendingPayments.filter(function(elem){ return elem.id === studentId; })[0].paymentType = 3;
+      $("#latePayment" + studentId).attr("type", "date");
+    };
+
+    $scope.toggleLatePaymentOptions = function(studentId){
+      var section = $("#latePaymentOption" + studentId);
+      if(section.css("display") === "none"){
+        section.css("display", "");
+      } else {
+        section.css("display", "none");
+      }
+    };
+
     if($routeParams.courseId){
-      $scope.selectCourse($routeParams.courseId);
+      $scope.selectCourse(parseInt($routeParams.courseId));
     }
   }]
 );
@@ -118,13 +188,13 @@ app.factory('PeopleService', [function(){
   return {
     getStudents: function(){
       return [
-        {"id": 1, "name": "Susana Alvarado", "enrollments": [{"id": 1, "name": "Curso II"}], "reserves": [{"id": 2, "name": "Curso IV"}], "previous": [{"id": 3, "name": "Curso I"}]},
-        {"id": 2, "name": "Noel Soria", "enrollments": [{"id": 1, "name": "Curso I"}]}
+        {"id": 1, "name": "Susana Alvarado", "hasScholarship": true, "scholarshipPercentage": 45, "debt": 145, "enrollments": [{"id": 1, "name": "Curso II"}], "reserves": [{"id": 2, "name": "Curso IV"}], "previous": [{"id": 3, "name": "Curso I"}], "debts": [{"id": 1, "course": {"name": "Biomagnetismo"}, "amount": 145, "dateLimit": "10 de octubre 2014"},{"id": 2, "course": {"name": "Homeopatia"}, "amount": 15}]},
+        {"id": 2, "name": "Noel Soria", "hasScholarship": false, "scholarshipPercentage": 0, "debt": 100, "debtLimit": "5 de enero 2015", "enrollments": [{"id": 1, "name": "Curso I"}], "debts": [{"id": 2, "course": {"name": "Biomagnetismo"}, "amount": 145, "dateLimit": "10 de octubre 2014", }]}
       ];
     },
 
     getStudent: function(id){
-      return this.getStudents().filter(function(elem){ return elem.id === id; });
+      return this.getStudents().filter(function(elem){ return elem.id === id; })[0];
     },
   };
 }]);
@@ -148,12 +218,12 @@ app.factory('CourseService', [function(){
     },
     getCourses: function(){
       return [
-        {"id": 1, "name": "Homeopatia II", "begin": "4 de junio 2014", "end": "5 de enero 2015", "schedule": "Jueves 18:00", "students": [{"id": 1, "name": "Susana Alvarado"}, {"id": 2, "name": "Alfredo Alvarado"}]},
-        {"id": 2, "name": "Biomagnetismo", "begin": "23 de octubre 2014", "end": "5 de enero 2015", "schedule": "Jueves 18:00", "students": [{"id": 1, "name": "Susana Alvarado"}, {"id": 3, "name": "Carlos Soria"}]},
-        {"id": 3, "name": "Curso I", "begin": "17 de abril 2014", "end": "5 de enero 2015", "schedule": "Jueves 18:00", "students": [{"id": 2, "name": "Alfredo Alvarado"}, {"id": 3, "name": "Carlos Soria"}]},
-        {"id": 4, "name": "Curso II", "begin": "6 de enero 2014", "end": "5 de enero 2015", "schedule": "Jueves 18:00", "students": [{"id": 3, "name": "Carlos Soria"}]},
-        {"id": 5, "name": "Curso III", "begin": "20 de marzo 2014", "end": "5 de enero 2015", "schedule": "Jueves 18:00", "students": [{"id": 2, "name": "Alfredo Alvarado"}]},
-        {"id": 6, "name": "Curso IV", "begin": "30 de septiembre 2014", "end": "5 de enero 2015", "schedule": "Jueves 18:00", "students": [{"id": 1, "name": "Susana Alvarado"}]}
+        {"id": 1, "code": "HOM-JU", "name": "Homeopatia II", "begin": "4 de junio 2014", "end": "5 de enero 2015", "schedule": "Jueves 18:00", "students": [{"id": 1, "name": "Susana Alvarado"}, {"id": 2, "name": "Alfredo Alvarado"}]},
+        {"id": 2, "code": "BIO-JU", "name": "Biomagnetismo", "begin": "23 de octubre 2014", "end": "5 de enero 2015", "schedule": "Jueves 18:00", "students": [{"id": 1, "name": "Susana Alvarado"}, {"id": 3, "name": "Carlos Soria"}]},
+        {"id": 3, "code": "CUR-I", "name": "Curso I", "begin": "17 de abril 2014", "end": "5 de enero 2015", "schedule": "Jueves 18:00", "students": [{"id": 2, "name": "Alfredo Alvarado"}, {"id": 3, "name": "Carlos Soria"}]},
+        {"id": 4, "code": "CUR-II", "name": "Curso II", "begin": "6 de enero 2014", "end": "5 de enero 2015", "schedule": "Jueves 18:00", "students": [{"id": 3, "name": "Carlos Soria"}]},
+        {"id": 5, "code": "CUR-III", "name": "Curso III", "begin": "20 de marzo 2014", "end": "5 de enero 2015", "schedule": "Jueves 18:00", "students": [{"id": 2, "name": "Alfredo Alvarado"}]},
+        {"id": 6, "code": "CUR-IV", "name": "Curso IV", "begin": "30 de septiembre 2014", "end": "5 de enero 2015", "schedule": "Jueves 18:00", "students": [{"id": 1, "name": "Susana Alvarado"}]}
       ];
     },
 
