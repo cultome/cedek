0037a589b4cdcd96c12e3f9a05eb984d0140a526
Carlos Soria <cultome@gmail.com>
2014-09-15 14:52:53 -0500

    Second round of e2e testing in courses

--- spec/javascript/course_spec.js
+++ spec/javascript/course_spec.js
@@ -7,23 +7,78 @@ describe('Course', function(){
     browser.get("http://localhost:92/app/");
   });
 
-  it('creates a new course', function(){
-    var before = countCoursesListed();
-    createCourse("Homeopatia");
-    var after = countCoursesListed();
-    expect(after).toBeGreaterThan(before);
+  describe("creating", function(){
+    it('creates a new course', function(){
+      var before = countCoursesListed();
+      createCourse("Homeopatia");
+      var after = countCoursesListed();
+      expect(after).toBeGreaterThan(before);
+    });
+
+    it("enroll student", function(){
+      createStudent("Susana Alvarado");
+      createCourse("Hipnosis");
+
+      var beforeCount = element(by.id("enrolledStudentsCount")).getText();
+      var beforeStudents = element.all(by.repeater("student in students")).count();
+
+      element.all(by.css(".students-panel")).last().element(by.cssContainingText("option", "Susana Alvarado")).click();
+      element(by.id("enrollBtn")).click();
+
+      var afterCount = element(by.id("enrolledStudentsCount")).getText();
+      var afterStudents = element.all(by.repeater("student in students")).count();
+
+      expect(afterCount).toBeGreaterThan(beforeCount);
+      expect(afterStudents).toBeGreaterThan(beforeStudents);
+    });
+
+    it("give scholarship", function(){
+      createStudent("Noel Soria");
+      createCourse("Astrologia");
+
+      var beforeCount = element(by.id("courseScholarshipsCount")).getText();
+      var beforeStudents = element.all(by.repeater("scholarship in scholarships")).count();
+
+      element.all(by.css(".scholarship-panel")).last().element(by.cssContainingText("option", "Noel Soria")).click();
+      element(by.model("panels.scholarship.amount")).sendKeys("50");
+      element(by.id("giveScholarshipBtn")).click();
+
+      var afterCount = element(by.id("courseScholarshipsCount")).getText();
+      var afterStudents = element.all(by.repeater("scholarship in scholarships")).count();
+
+      expect(afterCount).toBeGreaterThan(beforeCount);
+      expect(afterStudents).toBeGreaterThan(beforeStudents);
+    });
+
+    it("check attendance", function(){
+      createStudent("Carlos Soria");
+      createCourse("Introduccion a la Mediumnidad");
+      // nos inscribimos al curso
+      element.all(by.css(".students-panel")).last().element(by.cssContainingText("option", "Carlos Soria")).click();
+      element(by.id("enrollBtn")).click();
+      // vamos a los detalles del curso
+      element(by.id("goToDetailsBtn")).click();
+      // pnemos asistencia
+      element.all(by.css(".attendance-btn")).first().click();
+      // volvemos a la pagina de detalles y buscamos las sistencias
+      var course = selectLastCourse();
+      course.all(by.tagName("span")).first().click();
+      element(by.model("attendanceDate")).all(by.tagName("option")).last().click();
+      element(by.id("getAttendanceBtn")).click();
+      var count = element(by.css(".searchAttendance-panel")).all(by.repeater("student in students")).count();
+      browser.sleep(3000);
+      expect(count).toBeGreaterThan(0);
+    });
   });
 
   describe("listing", function(){
-    beforeEach(function(){
-      createCourse("Esperanto");
-    });
-
     it("all courses", function(){
+      createCourse("Esperanto");
       expect(countCoursesListed()).not.toEqual(0);
     });
 
     it("show enrolled students", function(){
+      createCourse("Curso I");
       var course = selectLastCourse();
       expect(element(by.css(".students-panel")).getCssValue("display")).toEqual("none");
       course.element(by.css(".courseStudents-btn")).click();
@@ -31,6 +86,7 @@ describe('Course', function(){
     });
 
     it("show attendance", function(){
+      createCourse("Psicologia");
       var course = selectLastCourse();
       expect(element(by.css(".attendance-panel")).getCssValue("display")).toEqual("none");
       course.element(by.css(".attendance-btn")).click();
@@ -38,34 +94,18 @@ describe('Course', function(){
     });
 
     it("send to details", function(){
+      createCourse("Taller de Homeopatia");
       var course = selectLastCourse();
       course.element(by.css(".courseDetails-btn")).click();
       expect(element(by.css(".studentListFullOpc")).isPresent()).toBe(true);
     });
   });
-/*
-  describe("details", function(){
-    beforeEach(function(){
-      // creamos un estudiante
-      addStudent("Noel Soria");
-      // creamos un curso y vamos a los detalles
-      createCourse("Biomagnetismo");
-      var course = selectLastCourse();
-      course.element(by.css(".courseDetails-btn")).click();
-    });
-
-    it("", function(){
-    });
-  });
-*/
-
-
 
 
   /*
    * Utilitarios
    */
-  function addStudent(studentName){
+  function createStudent(studentName){
     var before = countPeopleListed();
     goTo("addPeople");
     element(by.model("student.name")).sendKeys(studentName);
@@ -73,8 +113,8 @@ describe('Course', function(){
     element(by.model("student.address")).sendKeys("Av. del Pirul 365, Xochimilco, D.F.");
     element(by.model("student.email")).sendKeys("salsero2008@gmail.com");
     element(by.id("createPersonBtn")).click();
+    expect(element(by.id("buscarPersonas")).isPresent()).toBe(true);
     var students = element.all(by.repeater("student in students | filter:name"));
-    expect(students.isPresent()).toBe(true);
     var after = countPeopleListed();
     expect(students.count()).toBeGreaterThan(before);
   }
@@ -116,12 +156,40 @@ describe('Course', function(){
     goTo("addCourse");
     // llenamos el fornulario
     element(by.model("course.name")).sendKeys(courseName);
-    element(by.model("course.code")).sendKeys("HOM-JU");
     element(by.model("course.cost")).sendKeys("60");
-    element(by.cssContainingText("option", "Jueves")).click();
     element(by.model("course.hour")).sendKeys("1800");
     element(by.model("course.begin")).sendKeys("01012014");
     element(by.model("course.end")).sendKeys("12312014");
+    switch(new Date().getDay()){
+      case 0:
+        element(by.cssContainingText("option", "Domingo")).click();
+        element(by.model("course.code")).sendKeys("CUR-DOM");
+        break;
+      case 1:
+        element(by.cssContainingText("option", "Lunes")).click();
+        element(by.model("course.code")).sendKeys("CUR-LUN");
+        break;
+      case 2:
+        element(by.cssContainingText("option", "Martes")).click();
+        element(by.model("course.code")).sendKeys("CUR-MAR");
+        break;
+      case 3:
+        element(by.cssContainingText("option", "Miercoles")).click();
+        element(by.model("course.code")).sendKeys("CUR-MIE");
+        break;
+      case 4:
+        element(by.cssContainingText("option", "Jueves")).click();
+        element(by.model("course.code")).sendKeys("CUR-JUE");
+        break;
+      case 5:
+        element(by.cssContainingText("option", "Viernes")).click();
+        element(by.model("course.code")).sendKeys("CUR-VIE");
+        break;
+      case 6:
+        element(by.cssContainingText("option", "Sabado")).click();
+        element(by.model("course.code")).sendKeys("CUR-SAB");
+        break;
+    }
     element(by.css(".btn-block")).click();
     // checamos que se haya actualizado la pagina
     expect(element.all(by.css(".attendance-panel")).first().getText()).toEqual("Asistencia");
