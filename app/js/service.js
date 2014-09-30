54ba5030307689450712e6a52feba64188501224
Carlos Soria <cultome@gmail.com>
2014-09-30 12:44:42 -0500

    Change endpoint to a non reserved port

--- app/js/service.js
+++ app/js/service.js
@@ -1,7 +1,7 @@
 /* jshint strict: true */
 var app = angular.module('CEDEK');
 
-var serviceEndpoint = "http://localhost";
+var serviceEndpoint = "http://localhost:1234";
 
 app.factory('PeopleService', ['$resource', function($resource){
   "use strict";
